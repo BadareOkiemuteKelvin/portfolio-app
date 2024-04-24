37b353088912f37de068dd34776fd759543c3d29
BadareOkiemuteKelvin <badareokiemute201@gmail.com>
2024-04-24 08:01:50 +0100

    created home container-1

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -1,12 +1,17 @@
 import React from "react"
 import Header from "../../Views/Header/Header";
-
+import './Home.css'
 
 const Home =()=>{
 
  return(
     <div>
         <Header  />
+          <div className="container-1">
+            <h1>👨‍💻Web & Software Developer</h1>
+
+
+          </div>
    
  
     </div>
