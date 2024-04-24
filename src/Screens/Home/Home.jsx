a30a0f3d0ec016d26592bf9283fa8849048b376a
BadareOkiemuteKelvin <badareokiemute201@gmail.com>
2024-04-24 08:27:32 +0100

    updated container 1

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -9,6 +9,9 @@ const Home =()=>{
         <Header  />
           <div className="container-1">
             <h1>👨‍💻Web & Software Developer</h1>
+            <p>I build websites and mobile apps(iOS and Android) with Javascript,React.js,Nodejs,React Native & Expo.</p>
+            <p>I'm from Republic of Nigeria, Africa and am currently lead developer at bloomzon iouplc and codecamp academy.</p>
+            <p>Find me on <a href="https://twitter.com/@MelvincodeJs">X(formerly twitter)</a>, <a href="https://github.com/badareokiemutekelvin">GitHub</a>, Facebook and Instagram.</p>
 
 
           </div>
