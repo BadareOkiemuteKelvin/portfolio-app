6e7457b6da7c3ba9c613120e1eb35a2767ac1a43
BadareOkiemuteKelvin <badareokiemute201@gmail.com>
2024-05-01 08:42:56 +0100

    updated home page article preview

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -7,27 +7,29 @@ const Articles = [{
    "heading":"Javascript Security: Understanding Policies & Gates",
    "language":"javascript",
    "date":"26 April 2024",
-  "body":"What's a policy? What's a gate? It can be difficult understanding the difference between the two. Let's go over which does what and how to apply them to your Laravel app."
+  "body":"In the fast-paced world of web development, security remains a top priority, especially when dealing with JavaScript, which powers the majority of interactive and dynamic websites.",
+  "fullbody":""
+
 },{ "id":2,
-"heading":"Javascript Security: Understanding Policies & Gates",
+"heading":"React Routing: React router main concept and usage for developers",
 "language":"React.js",
 "date":"26 April 2024",
-"body":"What's a policy? What's a gate? It can be difficult understanding the difference between the two. Let's go over which does what and how to apply them to your Laravel app."},
+"body":"React Router is an indispensable tool in the toolbox of a modern web developer working with React applications. It enables the navigation among different components in a React application, mimicking the experience of multi-page web applications. "},
 { "id":3,
-"heading":"Javascript Security: Understanding Policies & Gates",
+"heading":"React Native: Native UI routing and statemanagement",
 "language":"React Native",
 "date":"26 April 2024",
-"body":"What's a policy? What's a gate? It can be difficult understanding the difference between the two. Let's go over which does what and how to apply them to your Laravel app."
+"body":"In the world of app development, managing the user interface (UI) and navigating between different screens (routing) are central to creating a fluid, intuitive user experience. Similarly, state management ensures that the UI behaves as expected as users interact with the app. "
 },{ "id":4,
-"heading":"Javascript Security: Understanding Policies & Gates",
-"language":"React.js",
+"heading":"Non-Blocking I/O: Nodejs Robust structure for modern days Web server",
+"language":"Nodejs",
 "date":"26 April 2024",
-"body":"What's a policy? What's a gate? It can be difficult understanding the difference between the two. Let's go over which does what and how to apply them to your Laravel app."},
+"body":"Node.js has revolutionized the way web servers are built by offering a non-blocking, event-driven architecture that is especially suited to handling numerous concurrent connections, a common requirement in today's digital world. This JavaScript runtime enables developers to build scalable and efficient web applications with ease."},
 { "id":5,
-"heading":"Javascript Security: Understanding Policies & Gates",
-"language":"React Native",
+"heading":"API Design: Understanding API routing and security in express.js",
+"language":"Express",
 "date":"26 April 2024",
-"body":"What's a policy? What's a gate? It can be difficult understanding the difference between the two. Let's go over which does what and how to apply them to your Laravel app."
+"body":"Express.js is a fast, unopinionated, minimalist web framework for Node.js that is widely used for building web applications and APIs. It simplifies routing, middleware, and server-side logic, but with this simplicity comes the need to understand and implement robust security practices, especially when dealing with API routes."
 }
 ]
 
@@ -53,10 +55,14 @@ const Home =()=>{
             <div key={data.id} id="articlecontainer">
                <h1>{data.heading}</h1>
                <div>
-               <p>{data.language}</p>
-               <p>{data.date}</p>
+               <p id="language">{data.language}</p>
+               <p id="date">{data.date}</p>
+               <p id="min-read">{(Number((data.body).length)/100).toFixed(0)} mins read</p>
                </div>
                <p>{data.body}</p>
+
+               <p id="readmore"> {`Read Article >`}</p>
+               <hr></hr>
              
 
 
