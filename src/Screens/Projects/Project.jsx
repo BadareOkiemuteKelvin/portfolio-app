ae376b4919bd0d96fe58f583f600c8be146d4bde
BadareOkiemuteKelvin <badareokiemute201@gmail.com>
2024-05-08 08:58:18 +0100

    created project items

--- src/Screens/Projects/Project.jsx
+++ src/Screens/Projects/Project.jsx
@@ -2,6 +2,34 @@ import React from "react"
 import Header from "../../Views/Header/Header";
 import './Project.css'
 import Footer from "../../Views/Footer/footer";
+import image1 from './assets/freshapp.png'
+
+const projects = [{
+  "id":1,
+ "image":image1,
+ "header":"Fresh App",
+ "description":"Fresh Food delievery app for managing customers request for various types of orders",
+ "Apptype":"Mobile Native App",
+ "Langauge":"React Native, Expo and JavaScript",
+ "link":"https://apps.apple.com/ng/app/bloomzon/id6464309937",
+},{
+ "id":2,
+"image":"",
+"header":"Wedlock",
+"description":"Wedding planner app with tools for managing guests,guest list and task.",
+"Apptype":"Web App Design",
+"Langauge":"Laravel Development",
+"link":"",
+},{
+ "id":3,
+"image":"",
+"header":"Wedlock",
+"description":"Wedding planner app with tools for managing guests,guest list and task.",
+"Apptype":"Web App Design",
+"Langauge":"Laravel Development",
+"link":"",
+}]
+
 
 
 const Project =()=>{
@@ -10,11 +38,30 @@ const Project =()=>{
     <div className="container">
         <main>
           <Header menuOption="project" />
-          <p>Hello Project</p>
+          <h1>Projects</h1>
+          <p className="p1">Some of my spare time tinkering</p>
+          {
+            projects.map((data)=><div key={data.id}>
+              <img src={data.image} alt="projectimage" />
+              <h2>{data.header}</h2>
+              <p className="p2">{data.description}</p>
+              <p className="p3">{">"}{" "}{data.Apptype}</p>
+              <p className="p3">{">"}{" "}{data.Langauge}</p>
+              <div className="div2"><a href={data.link}><p>Visit App</p></a></div>
+          
+
+            </div>)
+          }
+              
+
+
+
+
+
+
+
 
         </main>
-   
-
         <Footer />
     </div>
  )
---
+++ src/Screens/Projects/data.js
@@ -0,0 +1,26 @@
+
+const projects = [{
+     "id":1,
+    "image":'./assets/freshapp.png',
+    "header":"Wedlock",
+    "description":"Wedding planner app with tools for managing guests,guest list and task.",
+    "Apptype":"Web App Design",
+    "Langauge":"Laravel Development"
+},{
+    "id":2,
+   "image":"",
+   "header":"Wedlock",
+   "description":"Wedding planner app with tools for managing guests,guest list and task.",
+   "Apptype":"Web App Design",
+   "Langauge":"Laravel Development"
+},{
+    "id":3,
+   "image":"",
+   "header":"Wedlock",
+   "description":"Wedding planner app with tools for managing guests,guest list and task.",
+   "Apptype":"Web App Design",
+   "Langauge":"Laravel Development"
+}]
+
+
+export default projects
