fba16f83dc64048c338e856c43f08fafc3b9d5fe
BadareOkiemuteKelvin <badareokiemute201@gmail.com>
2024-05-10 08:09:21 +0100

    updated contact  page

--- src/Screens/Contact/Contact.jsx
+++ src/Screens/Contact/Contact.jsx
@@ -12,7 +12,36 @@ const Contact =()=>{
 
           <h1 id="contactheading">Contact Me</h1>
           <p id="message">if you'd like to chat about a project or just have question,please fill in the form below. I aim to get back within 2 days.</p>
-        
+         
+          <form>
+            <label>Name <span>*</span></label>
+            <br></br>
+            <input type="text"  />
+            <br></br>
+            <br></br>
+
+
+            <label>Email<span>*</span></label>
+            <br></br>
+            <input type="email" />
+            <br></br>
+            <br></br>
+            
+
+            <label>Message<span>*</span></label>
+            <br></br>
+            <input  type="text" id="messageBox"  />
+            <br></br>
+            <br></br>
+
+            <div id="buttonBox">
+                 <p>Send Message</p>
+            </div>
+
+
+
+
+          </form>
         
 
    
